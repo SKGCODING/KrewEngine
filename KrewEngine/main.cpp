642db39151f683145ac42f8dbc65dad4c6bfe38d
Sejja <diego.revilla.r@gmail.com>
2018-06-17 21:49:02 +0200

    InitialComit

---
+++ KrewEngine/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  KrewEngine
+//
+//  Created by Diego Revilla Rubiera on 17/06/2018.
+//  Copyright © 2018 Diego Revilla Rubiera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "KrewEngine Launched Sucessfully!\n";
+    return 0;
+}
+
+//We Will Use Vector Calculus With OpenGL
